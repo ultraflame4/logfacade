8ef06beaf0784980c1da348dbf5e793c74388f18
ultr42 <ultraflame4@gmail.com>
2025-03-02 16:55:17 +0800

    test

--- xmake.lua
+++ xmake.lua
@@ -7,3 +7,21 @@ target("logfacade")
     add_rules("utils.install.cmake_importfiles")
     add_rules("utils.install.pkgconfig_importfiles")
     add_packages("fmt")
+
+
+
+package("logfacade")
+    set_kind("library", {headeronly = true})
+    set_description("The logfacade package")
+
+    add_deps("vcpkg::fmt")
+
+    on_load(function (package)
+        package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
+    end)
+
+    on_fetch(function (package)
+        local result = {}
+        result.includedirs = package:installdir("include")
+        return result
+    end)
