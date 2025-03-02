eb014684e63d649b534cb763779fc2bf6c3e9590
ultr42 <ultraflame4@gmail.com>
2025-03-02 17:01:48 +0800

    AH

--- xmake.lua
+++ xmake.lua
@@ -10,18 +10,23 @@ target("logfacade")
 
 
 
-package("logfacade")
+    package("logfacade")
     set_kind("library", {headeronly = true})
     set_description("The logfacade package")
-
     add_deps("vcpkg::fmt")
 
-    on_load(function (package)
-        package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
+    add_urls("https://github.com/ultraflame4/logfacade.git")
+    add_versions("1.0", "8ef06beaf0784980c1da348dbf5e793c74388f18")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
     end)
 
-    on_fetch(function (package)
-        local result = {}
-        result.includedirs = package:installdir("include")
-        return result
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
     end)
